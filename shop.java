79d975f5c48263e578c67a38bc4bf41b9d5c26f9
Temiawaye <awayetemiloluwa@gmail.com>
2024-11-21 22:24:35 +0100

    first

--- shop.java
+++ shop.java
@@ -3,24 +3,58 @@ import java.util.Scanner;
 public class shop {
     public static void main(String[] args) {
         Scanner shop = new Scanner(System.in);
-        System.out.println("Enter Customer Name");
-        String customerName = shop.nextLine();
 
-        System.out.println("Enter Product");
-        String Product = shop.nextLine();
+        final int numofCustomer = 9;
 
-        System.out.println("Enter Price");
-        int price = shop.nextInt();
+        for (int i = 0; i < numofCustomer; i++) {
+            System.out.println("Enter Customer " + (i + 1) + " Name:"  );
+            String customerName = shop.nextLine();
 
-        System.out.println("Enter Qty");
-        int Qty = shop.nextInt();
+            System.out.println("Enter Product:");
+            String Product = shop.nextLine();
 
-        System.out.println("RECEIPT");
-        System.out.println("Customer Name: " + customerName);
-        System.out.println("Order: " + Product);
-        System.out.println("Quantity: " + Qty);
-        System.out.println("Price: " + price);
-        System.out.println(Qty * price);
+            System.out.println("Description:");
+            String description = shop.nextLine();
+
+            System.out.println("Enter Price:");
+            int price = shop.nextInt();
+
+            System.out.println("Enter Qty:");
+            int Qty = shop.nextInt();
+
+            System.out.println("-----------------------------------------------");
+
+            System.out.println("RECEIPT FOR CUSTOMER " + (i + 1));
+            System.out.println("Customer Name: " + customerName);
+            System.out.println("Order: " + Product);
+            System.out.println("Description: " + description);
+            System.out.println("Quantity: " + Qty);
+            System.out.println("Price: " + price);
+            System.out.println("Total: " + Qty * price);
+            System.out.println("-----------------------------------------------");
+            System.out.println(" ");
+
+            shop.nextLine(); // Consume newline character
+        }
+
+        // System.out.println("Enter Customer Name");
+        // String customerName = shop.nextLine();
+
+        // System.out.println("Enter Product");
+        // String Product = shop.nextLine();
+
+        // System.out.println("Enter Price");
+        // int price = shop.nextInt();
+
+        // System.out.println("Enter Qty");
+        // int Qty = shop.nextInt();
+
+        // System.out.println("RECEIPT");
+        // System.out.println("Customer Name: " + customerName);
+        // System.out.println("Order: " + Product);
+        // System.out.println("Quantity: " + Qty);
+        // System.out.println("Price: " + price);
+        // System.out.println(Qty * price);
 
 
     }
